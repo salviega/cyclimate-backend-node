1d9991526318ee983e0ca02dbcb3155b124fef2a
salviega <salviega6@gmail.com>
2023-02-05 22:54:38 -0500

    [DOCS] README created 📝

--- index.js
+++ index.js
@@ -2,101 +2,32 @@ import axios from "axios";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import Moralis from "moralis";
-import { EvmChain } from "@moralisweb3/evm-utils";
 dotenv.config();
-const MORALIS_API_KEY = process.env.MORALIS_API_KEY;
-const moralis = Moralis.default;
-const chain = EvmChain.MUMBAI;
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-const fetchData = async (typeData) => {
-  let response;
-  switch (typeData) {
-    case "graphInformation":
-      response = await axios.get(
-        "http://ec2-34-207-93-96.compute-1.amazonaws.com:8088/last_data/?token=0ce956fc-131b-42d6-a4b1-8e8319e45f84"
-      );
-      return response.data;
-    case "counter":
-      response = await axios.get(
-        "http://ec2-34-207-93-96.compute-1.amazonaws.com:8088/query_drop/?token=0ce956fc-131b-42d6-a4b1-8e8319e45f84"
-      );
-      return response.data;
-    case "redeem":
-      response = await axios.get(
-        "http://ec2-34-207-93-96.compute-1.amazonaws.com:8088/claim_drop/?token=0ce956fc-131b-42d6-a4b1-8e8319e45f84&user=user2"
-      );
-      console.log(response.data);
-      return response.data;
-    case "burned":
-      response = {
-        cid: "UNO",
-        ret_url: "DOS",
-        claim: 2,
-      };
-      console.log(response);
-      return response;
-    default:
-      return [];
-  }
-};
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/lastest", async (req, res) => {
-  const data = await fetchData("graphInformation");
-  res.set("Access-Control-Allow-Origin", "*");
-  res.send(data);
-});
-
-app.get("/counter", async (req, res) => {
-  const data = await fetchData("counter");
-  res.send({ data });
-});
-
-app.get("/redeem", async (req, res) => {
-  const data = await fetchData("redeem");
-  res.send({ data });
-});
-
-app.get("/burned", async (req, res) => {
-  const data = await fetchData("burned");
-  res.send({ data });
-});
-
-app.get("/allNFTs", async (req, res) => {
+app.get("/getData", async (req, res) => {
   const { query } = req;
+  console.log(query);
   const wallet = query.wallet;
 
-  const response = await moralis.EvmApi.nft.getNFTOwners({
-    address: query.address,
-    chain: chain,
-  });
-  const result = response.raw;
-  const NFTs = result.result;
-  const NFTsByOwer = (wallet) => {
-    const NFTsOwner = [];
-    NFTs.map((NFT) => {
-      if (NFT.owner_of === wallet.toLocaleLowerCase()) {
-        NFTsOwner.push(NFT);
-      }
-    });
-    return NFTsOwner;
-  };
+  const response = await axios.get(
+    "http://ec2-100-24-74-70.compute-1.amazonaws.com:8088/query_proposal/",
+    {
+      params: { wallet },
+    }
+  );
 
+  console.log("response.data: ", response.data);
+
+  //const data = await fetchData("getData");
   res.set("Access-Control-Allow-Origin", "*");
-  res.send(NFTsByOwer(wallet));
+  res.send(response.data);
 });
 
-moralis
-  .start({
-    apiKey: MORALIS_API_KEY,
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT} 🎈`);
-    });
-  });
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} 🎈`);
+});
